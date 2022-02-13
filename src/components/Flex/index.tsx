f11383d7f2b59a8b54c3c68cef9633aaae9ec1f8
azotov <zotovprog@gmail.com>
2022-02-13 19:47:53 +0300

    first commit

---
+++ src/components/Flex/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Component: React.FC = () => {
+  return (
+    <Container>
+      <h1>Test</h1>
+    </Container>
+  );
+};
+
+export default Component;
